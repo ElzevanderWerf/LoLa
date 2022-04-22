8123ad897bb73c8e487341171da73c0d9ebb1d4d
Elze van der Werf <e.g.vanderwerf@students.uu.nl>
2022-04-22 14:01:52 +0200

    add IO option to translate all sentences in input file to target language output file

--- Trans.hs
+++ Trans.hs
@@ -4,19 +4,46 @@ module Main where
 import "gf" PGF
 import TransProp
 import TransPropFunctions
+import System.Environment
+import System.IO
 
 main = do
   pgf <- readPGF "Prop.pgf"
-  interact (doTrans pgf)     -- translate the input proposition
+  args <- getArgs
+  if (length args) == 1    -- stack run trans <proposition>
+    then do putStrLn (doTrans pgf (args !! 0))
+    else do                -- stack run trans <source-language> <input-file> <target-language> <output-file>
+      f <- readFile (args !! 1)
+      let flines = lines f
+      let sourcelang = mkCId (args !! 0)
+      let targetlang = mkCId (args !! 2)
+ 
+      -- Translate and write to output file
+      outh <- openFile (args !! 3) WriteMode
+      let doTransPGF = doTransFromTo pgf sourcelang targetlang
+      hPutStrLn outh (unlines (map doTransPGF flines))
+      hClose outh
 
+      putStrLn "done"
+    
 -- Parse the input string in all languages and translate it with all 
 -- translation modes into all languages 
 doTrans pgf s = case parseAllLang pgf (startCat pgf) s of 
   (sourceL,ts):_ -> unlines [display m t | t <- ts, noFreeVars t, m <- [MNone, MOptimize, MNormalize, MMinimalize, MSimplify, MCheckLaw]] 
   --(sourceL,ts):_ -> unlines [display m t | t <- ts, noFreeVars t, m <- [MSimplify]] --Debug: print tree
-  _ -> "no parse\n"
+  _              -> "no parse\n"
  where
    display m t = unlines $ (showExpr [] t) :                 -- print the tree
      (show m ++ ":") :                                       -- print the mode
        [transfer m pgf la t | la <- languages pgf]           -- print the translations (for each language)
        --[unlines (map show (transfer m pgf la t)) | la <- languages pgf]      -- Debug: print tree
+
+-- Parse the input string in the source language and translate it with
+-- AST simplification into the target language
+doTransFromTo pgf source_l target_l s = case parse pgf source_l (startCat pgf) s of 
+  ts | length ts > 0 -> unlines [transfers t | t <- ts, noFreeVars t]    -- this assumes the input sentences are parsable
+   where
+     transfers t = 
+       if (noFreeVars t) then transfer MSimplify pgf target_l t else "contains free variables"  --TODO last case doesn't work
+  _  -> "no parse"
+--TODO: zorg dat als de tree free variables heeft, dat dat ook zichtbaar wordt in de vertaling
