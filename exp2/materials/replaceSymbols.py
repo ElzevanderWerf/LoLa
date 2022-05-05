043d0b32e2b1103bb8e7b1db3af0205ce99540f7
Elze van der Werf <e.g.vanderwerf@students.uu.nl>
2022-05-05 14:28:59 +0200

    copied from cade-2011-study-1

---
+++ exp2/materials/replaceSymbols.py
@@ -0,0 +1,16 @@
+"""Module for replacing GGC symbols with common FOL symbols and changing bulleting"""
+        
+def replace_symbols(s):
+    """
+    Replaces the GGC symbols in input string s with common FOL symbols
+    """
+    dct = {126:172,
+       38:8743,
+       124:8744,
+       36:8594,
+       64:8704,
+       47:8707}
+    return s.translate(dct)
+
+def replace_bulleting(s):
+    return s.replace(r'\item', r'\n\t'+chr(8226)) #TODO enter erna
