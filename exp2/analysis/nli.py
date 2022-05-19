3cd5796628681f605b6dd30a6413ca8868c7582e
Elze van der Werf <e.g.vanderwerf@students.uu.nl>
2022-05-19 10:17:00 +0200

    Code cleanup

--- exp2/analysis/nli.py
+++ exp2/analysis/nli.py
@@ -2,56 +2,47 @@
 in the survey."""
 import pandas as pd
 import numpy as np
+from itertools import chain
 from scipy.stats.stats import ttest_ind
 
-# TODO change file names
+# TODO change file names. Import results
 df1 = pd.read_csv("../results/CSVs/ex1.csv", header=0)
 df2 = pd.read_csv("../results/CSVs/ex2.csv", header=0)
 df3 = pd.read_csv("../results/CSVs/ex3.csv", header=0)
 
 DFs = [df1, df2, df3]
 
-lines = []
+lines = []  # The output
 participants = sum([len(df) for df in DFs])
 
+def lookUpNLI(df, indices):
+    nliQs = df.loc[:, df.columns.str.startswith("NLI")]
+    
+    #the answers per question
+    answers = [list(nliQs.iloc[:,i]) for i in indices] 
+    
+    #the percentage of correct answers per question
+    return [answers[q].count("Correct") / len(answers[q])
+                             for q in range(len(indices))]
 
+##############################################################################
 lines.append("COMPARISON BETWEEN THE 3 SYSTEMS")
-def lookUpRange(df,formulaType, rrange):
-    answersPerParticipant = df.loc[:,
-                  "NLI-"+formulaType+str(rrange[0]):
-                  "NLI-"+formulaType+str(rrange[-1])].to_numpy()
-    correctPercentagePerQ = []
-    for a in range(len(rrange)):
-        answers = [participant[a] for participant in answersPerParticipant]
-        correctPercentagePerQ.append(answers.count("Correct") / len(answers))
-    return correctPercentagePerQ
-
 # Question sets
-GGC1 = RG1 = range(0,7)
-GGC2 = RG2 = range(7,14)
-GGC3 = RG3 = range(14,20)
+(GGC1, GGC2, GGC3, RG1, RG2, RG3) = (range(0,7), range(7,14), range(14,21), 
+                                     range(21,28), range(28,35), range(35,42))
 
-baseline = lookUpRange(df1, "GGC", GGC1) + lookUpRange(
-    df3, "GGC", GGC2) + lookUpRange(
-    df2, "GGC", GGC3) + lookUpRange(
-    df1, "RG", RG1) + lookUpRange(
-    df3, "RG", RG2) + lookUpRange(
-    df2, "RG", RG3) 
-        
-rantaI = lookUpRange(df2, "GGC", GGC1) + lookUpRange(
-    df1, "GGC", GGC2) + lookUpRange(
-    df3, "GGC", GGC3) + lookUpRange(
-    df2, "RG", RG1) + lookUpRange(
-    df1, "RG", RG2) + lookUpRange(
-    df3, "RG", RG3) 
-        
-rantaII = lookUpRange(df3, "GGC", GGC1) + lookUpRange(
-    df2, "GGC", GGC2) + lookUpRange(
-    df1, "GGC", GGC3) + lookUpRange(
-    df3, "RG", RG1) + lookUpRange(
-    df2, "RG", RG2) + lookUpRange(
-    df1, "RG", RG3) 
-        
+def latinSquare(ordering):
+    return lookUpNLI(ordering[0], GGC1) + lookUpNLI(
+    ordering[1], GGC2) + lookUpNLI(
+    ordering[2], GGC3)+ lookUpNLI(
+    ordering[0], RG1)+ lookUpNLI(
+    ordering[1], RG2)+ lookUpNLI(
+    ordering[2], RG3)
+
+baseline = latinSquare([df1, df3, df2])
+rantaI = latinSquare([df2, df1, df3])
+rantaII = latinSquare([df3, df2, df1])
+
 # Averages
 lines.append("\tBaseline: {} percent correct".format(
     np.mean(baseline)))
@@ -68,22 +59,19 @@ lines.append("\tT-test Baseline vs RantaII: {}".format(ttest_ind(baseline, ranta
 lines.append("\tT-test RantaI vs RantaII: {}".format(ttest_ind(rantaI, rantaII)))
 
 
+##############################################################################
 lines.append("\n\nGGC VS RG FORMULAS")
-ggcPerDF = [lookUpRange(df1, "GGC", range(0,21)), 
-            lookUpRange(df2, "GGC", range(0,21)), 
-            lookUpRange(df3, "GGC", range(0,21))]
-ggc = [np.mean([ggcPerDF[0][i], ggcPerDF[1][i], ggcPerDF[2][i]]) for i in range(len(ggcPerDF[0]))]
+ggcPerDF = [lookUpNLI(df, list(chain(GGC1, GGC2, GGC3))) for df in DFs]
+ggc = [np.mean([ggcPerDF[0][i], ggcPerDF[1][i], ggcPerDF[2][i]]) 
+       for i in range(len(ggcPerDF[0]))]
 
-rgPerDF = [lookUpRange(df1, "RG", range(0,21)), 
-            lookUpRange(df2, "RG", range(0,21)), 
-            lookUpRange(df3, "RG", range(0,21))]
-rg = [np.mean([rgPerDF[0][i], rgPerDF[1][i], rgPerDF[2][i]]) for i in range(len(rgPerDF[0]))]
+rgPerDF = [lookUpNLI(df, list(chain(RG1, RG2, RG3))) for df in DFs]
+rg = [np.mean([rgPerDF[0][i], rgPerDF[1][i], rgPerDF[2][i]]) 
+      for i in range(len(rgPerDF[0]))]
 
 # Averages
-lines.append("\tGGC: {} percent correct".format(
-    np.mean(ggc)))
-lines.append("\tRG: {} percent correct".format(
-    np.mean(rg)))
+lines.append("\tGGC: {} percent correct".format(np.mean(ggc)))
+lines.append("\tRG: {} percent correct".format(np.mean(rg)))
         
 # T-tests
 # Mean proportion of correct answers per question. Then compare these
@@ -91,38 +79,26 @@ lines.append("\tRG: {} percent correct".format(
 lines.append("\n\tT-test GGC vs RG: {}".format(ttest_ind(ggc, rg)))
 
 
+##############################################################################
 lines.append("\n\nWB VS NWB FORMULAS")
 #TODO check difference between Ranta-I and Ranta-II
-def lookUpIndices(df, indices):
-    nliQs = df.loc[:, df.columns.str.startswith("NLI")]
-    answersPerQ = [list(nliQs.iloc[:,i]) for i in indices]
-
-    correctPercentagePerQ = []
-    for q in range(len(indices)):
-        correctPercentagePerQ.append(answersPerQ[q].count("Correct") / len(answersPerQ[q]))
-    return correctPercentagePerQ
-
+   
 nli_items = pd.read_csv("../materials/experimental_items/nli-items1.csv", header=0)
 WBness = nli_items.loc[:,"Well-behavedness"]
 
 WB_indices = [i for i in range(len(WBness)) if WBness[i] == "WB"]
 NWB_indices = [i for i in range(len(WBness)) if WBness[i] == "NWB"]
 
-WBPerDF = [lookUpIndices(df1, WB_indices), 
-            lookUpIndices(df2, WB_indices), 
-            lookUpIndices(df3, WB_indices)]
-NWBPerDF = [lookUpIndices(df1, NWB_indices), 
-            lookUpIndices(df2, NWB_indices), 
-            lookUpIndices(df3, NWB_indices)]
+WBPerDF = [lookUpNLI(df, WB_indices) for df in DFs]
+NWBPerDF = [lookUpNLI(df, NWB_indices) for df in DFs]
 
 WB = [np.mean([WBPerDF[0][i], WBPerDF[1][i], WBPerDF[2][i]]) for i in range(len(WBPerDF[0]))]
 NWB = [np.mean([NWBPerDF[0][i], NWBPerDF[1][i], NWBPerDF[2][i]]) for i in range(len(NWBPerDF[0]))]
 
 # Averages
-lines.append("\tWB: {} percent correct".format(
-    np.mean(WB)))
-lines.append("\tNWB: {} percent correct".format(
-    np.mean(NWB)))
+lines.append("\tWB: {} percent correct".format(np.mean(WB)))
+lines.append("\tNWB: {} percent correct".format(np.mean(NWB)))
+
 # T-tests
 # Mean proportion of correct answers per question. Then compare these
 # percentages per group
